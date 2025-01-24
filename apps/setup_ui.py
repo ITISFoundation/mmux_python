8b64d2dd09c0d5644b5f52cb95b50c5173292150
Javier Garcia Ordonez <ordonez@zmt.swiss>
2025-01-24 11:55:58 +0100

    header & cloning & pop-ups working

--- apps/setup_ui.py
+++ apps/setup_ui.py
@@ -1,85 +1,83 @@
 import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 from mmux_utils.funs_evaluate import create_run_dir
 from mmux_utils.funs_git import clone_repo, import_function_from_repo, check_repo_exists
 import time
 from pathlib import Path
-
-temp_dir = create_run_dir(Path("."), "setup")
+from styles import header_style
 
 
 def reset_clone_status():
     st.session_state.cloned = False
 
 
-if "cloned" not in st.session_state:
-    reset_clone_status()
-
-
-def clone(repo_url: str, temp_dir: Path):
+def clone(repo_url: str, temp_dir: Path, mssg_container: DeltaGenerator):
     try:
         assert check_repo_exists(repo_url)
+    except:
+        mssg_container.warning("Please enter a valid GitHub repository URL!")
 
+    with mssg_container:
         with st.spinner("Cloning..."):
             status, message = clone_repo(repo_url, target_dir=temp_dir)
             time.sleep(2)
 
+    with mssg_container.empty():
         if status == True:
+            ## NB: center-alignment doesnt seem to be natively possible in Streamlit
+            ## the only option is to use HTML in Markdown and set style='text-align: center; ..
             st.success("✔️ Cloning succeeded!")
             st.session_state.cloned = True  # Update session state
         else:
             st.error("❌ Cloning failed.")
             st.error(message)
-    except:
-        st.warning("Please enter a valid GitHub repository URL!")
+
+
+#### Define page style & setup   ###################################
+####################################################################
+# Set page configuration
+st.set_page_config(page_title="MetaModeling UX", layout="wide")
+st.markdown(header_style, unsafe_allow_html=True)  # Purple header
+st.markdown('<div class="purple-header">MetaModeling UX</div>', unsafe_allow_html=True)
+if "cloned" not in st.session_state:
+    reset_clone_status()
+## create folder where to run everything
+temp_dir = create_run_dir(Path("."), "setup")
+st.header("Pipeline Setup")
+####################################################################
+
+#
+
+#### Define page layout          ###################################
+#### PS this decouples layout & functionality!
+####################################################################
+(left_col, right_half) = st.columns([1, 1])  # Split into columns
+label_col, input_col, button_placeholder = left_col.columns(
+    [0.5, 3, 1],  # Create a row with columns for label, input & button
+    vertical_alignment="center",
+)
+####################################################################
 
 
 def main():
-    ## TODO rather create this as a pure python script (no Streamlit)
-    # and only then, move to making the GUI and see if it is powerful enough
-
-    st.title("MetaModeling UX")
-    st.header("Pipeline Setup")
-
-    repo_url = st.text_input(
-        "Model: ",
+    ## First element
+    label_col.text("Model: ")
+    repo_url = input_col.text_input(
+        label="Model Input",
+        label_visibility="collapsed",
         placeholder="https://github.com/...",
         on_change=lambda: reset_clone_status(),
     )
-
-    st.button(
-        "Clone",
+    lock = st.session_state.cloned and bool(repo_url)
+    button_placeholder.button(
+        label="Clone",
         on_click=clone,
-        args=(repo_url, temp_dir),
-        disabled=st.session_state.cloned,
+        args=(repo_url, temp_dir, left_col),
+        disabled=lock,
     )
-
-    print("After button?", st.session_state.cloned)
-
-    ## TODO try Copilot changes to make Clone button disappear once clicked; otherwise check what I saw in Overflow
-    ## also probably much better program flow, with proper callbacks and so on (not so much, nested ifs)
-    ## in any case, commit this version and be able to inspect diff solutions (in diff branches / commits?)
 
     ## TODO be able to extract input & output variables;
     # and generate the textboxes to input values for them
-
-    # if st.button("Run"):  ## TODO only show when cloning is complete
-    #     st.warning("Not implemented yet")
-
-    #####################
-
-    #         fun = import_function_from_repo(
-    #             repo_path, module_name="evaluation.py", function_name="evaluator"
-    #         )
-
-    # # Run button
-    # if st.button("Run"):
-    #     if clone_repo and success:
-    #         # Call backend logic
-    #         # result = process_input(user_input)
-    #         result = 0.0
-    #         st.success(f"Result: {result}")
-    #     else:
-    #         st.warning("Please enter some input!")
 
 
 if __name__ == "__main__":
---
+++ apps/styles.py
@@ -0,0 +1,13 @@
+header_style = """
+    <style>
+    .purple-header {
+        background-color: #460066;
+        color: white;
+        padding: 10px;
+        # font: Helveticcca;
+        font-size: 30px;
+        font-weight: bold;
+        text-align: center;
+        border-radius: 10px;
+    }</style>
+    """
